e254fe08887581a4216f512676d50bd968420c79
felipenino13 <cristianfelipenino@gmail.com>
2026-02-14 12:46:36 -0500

    primera version del sitio web de Cris

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,122 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-20 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="grid grid-cols-1 sm:grid-cols-2 items-center gap-30 text-center sm:items-start sm:text-left">
+          <div className="grid gap-8 m-auto">
+            <p>֎ crisnnino</p>
+            <div className="space-y-5">
+              <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                Soluciones web modernas y automatizaciones inteligentes.
+              </h1>
+              <p className="text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+                Ayudo a empresas y emprendedores a crear sitios web rápidos, escalables y optimizados para conversión.
+              </p>
+            </div>
+              <a
+                className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+                href="https://www.behance.net/dgneuta"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Portafolio
+              </a>
+          </div>
+          <div className="space-y-5 rounded-2xl border border-neutral-800 bg-neutral-950/40 p-6 shadow-sm">
+            
+          
+          {/* Contact Form */}
+            <form
+            action="mailto:info@crisnnino.com"
+            method="post"
+            encType="text/plain"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+            <div className="text-center">
+              <h1 className="text-xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                Hablemos de tu proyecto
+              </h1>
+              <p className="text-md leading-6 text-zinc-600 dark:text-zinc-400">
+                Cuéntame qué necesitas y te respondo lo antes posible.
+              </p>
+            </div>
+            
+
+            {/* Nombre */}
+            <div className="space-y-2">
+                <label htmlFor="name" className="text-sm text-neutral-200">
+                Nombre
+                </label>
+                <input
+                id="name"
+                name="Nombre"
+                type="text"
+                autoComplete="name"
+                placeholder="Tu nombre"
+                className="w-full rounded-xl border border-neutral-800 bg-neutral-950 px-4 py-3 text-neutral-100 placeholder:text-neutral-500 outline-none focus:border-neutral-600"
+                required
+                />
+            </div>
+
+            {/* Email */}
+            <div className="space-y-2">
+                <label htmlFor="email" className="text-sm text-neutral-200">
+                Email
+                </label>
+                <input
+                id="email"
+                name="Email"
+                type="email"
+                autoComplete="email"
+                placeholder="tu@email.com"
+                className="w-full rounded-xl border border-neutral-800 bg-neutral-950 px-4 py-3 text-neutral-100 placeholder:text-neutral-500 outline-none focus:border-neutral-600"
+                required
+                />
+            </div>
+
+            {/* Teléfono */}
+            <div className="space-y-2">
+                <label htmlFor="phone" className="text-sm text-neutral-200">
+                Número de celular
+                </label>
+                <input
+                id="phone"
+                name="Celular"
+                type="tel"
+                autoComplete="tel"
+                placeholder="+57 300 000 0000"
+                className="w-full rounded-xl border border-neutral-800 bg-neutral-950 px-4 py-3 text-neutral-100 placeholder:text-neutral-500 outline-none focus:border-neutral-600"
+                />
+            </div>
+
+            {/* Mensaje */}
+            <div className="space-y-2">
+                <label htmlFor="message" className="text-sm text-neutral-200">
+                Mensaje
+                </label>
+                <textarea
+                id="message"
+                name="Mensaje"
+                placeholder="Cuéntame sobre tu proyecto..."
+                rows={5}
+                className="w-full resize-none rounded-xl border border-neutral-800 bg-neutral-950 px-4 py-3 text-neutral-100 placeholder:text-neutral-500 outline-none focus:border-neutral-600"
+                required
+                />
+            </div>
+
+            {/* Botón */}
+            <div className="pt-2">
+                <button
+                type="submit"
+                className="w-full rounded-xl bg-white px-5 py-3 text-sm font-medium text-black hover:opacity-90 transition"
+                >
+                Enviar mensaje
+                </button>
+            </div>
+            </form>
+          </div>
+
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+          
       </main>
     </div>
   );
