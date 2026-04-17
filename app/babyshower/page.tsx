1c39526db67ed853f1a888fc150eb89e15c82eef
felipenino13 <cristianfelipenino@gmail.com>
2026-04-17 17:09:27 -0500

    se agrega el video del pollito

--- app/babyshower/page.tsx
+++ app/babyshower/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+export default function Baby() {
   return (
   <>
-    hola pagina del baby shower
+    <div className="grid grid-flow-col grid-rows-3 gap-4 flex justify-center bg-[#FFFFFF] h-dvh">
+        <div className="row-span-3">
+            <h1 className="text-[#000000]">hola bebe</h1>
+            <video 
+                src="/video/pollitoVideo.mp4"
+                controls
+                autoPlay
+                muted
+                loop
+                className="h-full w-auto"
+            ></video>
+        </div>
+    </div>
 </>
   );
 }
