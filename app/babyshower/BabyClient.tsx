3ccf5fcd4e68b77fddec5ed24d3953f4fdcf4b6c
felipenino13 <cristianfelipenino@gmail.com>
2026-04-18 15:27:37 -0500

    se hacen mas pequeño los estilos para mejorar la version en mobile

--- app/babyshower/BabyClient.tsx
+++ app/babyshower/BabyClient.tsx
@@ -40,30 +40,43 @@ export default function BabyClient({
             <div
               className={`${gluten.className} m-auto w-fit rounded-b-lg bg-[#0124C4]/50 p-4 text-white backdrop-blur-sm drop-shadow-lg`}
             >
-              <p className="text-center text-5xl font-[700] tracking-tight text-[#FFF662]">
+              <p className="text-center text-4xl font-[700] tracking-tight text-[#FFF662]">
                 16 MAY
               </p>
-              <p className="text-center text-2xl">3:00 p.m.</p>
+              <p className="text-center text-1xl">3:00 p.m. Funza</p>
             </div>
           </div>
 
-          <div>
-            <img
-              className="m-auto"
-              src="/img/tituloBabyShower.png"
-              alt="Título baby shower"
-              width="500"
-            />
+          <div className="relative">
+            <div className="relative z-20">
+                <img
+                className="m-auto -mb-10 z-10"
+                src="/img/tituloBabyShower.png"
+                alt="Título baby shower"
+                width="400"
+                />    
+            </div>
             <div
-              className={`${gluten.className} m-auto w-fit rounded-t-lg bg-[#C22B00]/50 p-4 text-white backdrop-blur-sm drop-shadow-lg`}
+              className={`${gluten.className} m-auto w-fit rounded-t-lg bg-[#C22B00]/50 p-4 text-white backdrop-blur-sm drop-shadow-lg z-0 text-center`}
             >
-              <p className="text-center text-2xl">Hola</p>
-              <p className="text-center text-5xl font-[700] tracking-tight text-[#FFF662]">
+              <p className="text-center text-1xl">Hola</p>
+              <p className="text-center text-4xl font-[700] tracking-tight text-[#FFF662]">
                 {name}
               </p>
-              <p className="text-center text-2xl">
+              <p className="text-center text-1xl">
                 Te invito a celebrar este día especial con mis papitos
               </p>
+              <button
+                type="button"
+                onClick={() =>
+                    window.open(
+                    "https://waze.com/ul?ll=4.7310472,-74.2218603&navigate=yes",
+                    "_blank"
+                    )
+                }
+                className={`${gluten.className} mb-3 rounded-xl bg-[#05C8F7] p-3 text-black w-fit`}>
+                    Confirmar asistencia
+                </button>
             </div>
           </div>
         </div>
@@ -75,22 +88,22 @@ export default function BabyClient({
         <button
           type="button"
           onClick={() => setOpenGiftModal(true)}
-          className="flex aspect-square items-center rounded-r-full bg-[#FFDF04] p-4 transition-transform duration-300 hover:scale-105"
+          className="flex aspect-square items-center rounded-r-full bg-[#FFDF04] p-3 transition-transform duration-300 hover:scale-105"
         >
           <div>
-            <Gift className="m-auto h-10 w-10 text-[#BA2800]/90" />
-            <p className="m-auto font-[700] text-[#BA2800]/70">Regalo</p>
+            <Gift className="m-auto h-7 w-7 text-[#BA2800]/90" />
+            <p className="m-auto font-[700] text-[#BA2800]/70 text-sm">Regalo</p>
           </div>
         </button>
 
         <button
           type="button"
           onClick={() => setOpenMapModal(true)}
-          className="flex aspect-square items-center rounded-l-full bg-[#FFDF04] p-4 transition-transform duration-300 hover:scale-105"
+          className="flex aspect-square items-center rounded-l-full bg-[#FFDF04] p-3 transition-transform duration-300 hover:scale-105"
         >
           <div>
-            <MapPin className="m-auto h-10 w-10 text-[#BA2800]/90" />
-            <p className="m-auto font-[700] text-[#BA2800]/70">Ubicación</p>
+            <MapPin className="m-auto h-7 w-7 text-[#BA2800]/90" />
+            <p className="m-auto font-[700] text-[#BA2800]/70 text-sm">Ubicación</p>
           </div>
         </button>
       </div>
@@ -109,12 +122,12 @@ export default function BabyClient({
             <h2
               className={`${gluten.className} mb-4 text-center text-2xl font-bold text-[#BA2800]`}
             >
-              Regalito
+              Regalo
             </h2>
 
-            <p className="text-center leading-relaxed text-gray-700">
+            <p className={`${gluten.className} mb-4 text-[#000000] text-center leading-relaxed text-gray-700`}>
               Para nosotros es muy importante tu compañía más que un regalo,
-              pero si quieres tener un detalle puedes traer pañales Winnie etapa{" "}
+              pero si quieres tener un detalle puedes traer pañales etapa{" "}
               <span className="font-semibold">{etapa}</span>, pañitos y{" "}
               <span className="font-semibold">{regalo}</span>.
             </p>
@@ -136,6 +149,10 @@ export default function BabyClient({
             <h2 className={`${gluten.className} mb-4 text-center text-2xl font-bold text-[#BA2800]`}>
               ¿Cómo quieres llegar?
             </h2>
+            <p className={`${gluten.className} mb-4 text-[#000000] text-center`}>
+                Casa de retiros hermanas Franciscanas<br></br>
+                Cra. 28b #13-12, Funza, Cundinamarca
+            </p>
 
             <button
               type="button"
@@ -145,9 +162,8 @@ export default function BabyClient({
                   "_blank"
                 )
               }
-              className="mb-3 w-full rounded-xl bg-[#0119B9] p-3 text-white"
-            >
-              Abrir en Google Maps
+              className={`${gluten.className} mb-3 w-full rounded-xl bg-[#0119B9] p-3 text-white`}>
+                Abrir en Google Maps
             </button>
 
             <button
@@ -158,9 +174,8 @@ export default function BabyClient({
                   "_blank"
                 )
               }
-              className="w-full rounded-xl bg-[#05C8F7] p-3 text-black"
-            >
-              Abrir en Waze
+              className={`${gluten.className} mb-3 w-full rounded-xl bg-[#05C8F7] p-3 text-black`}>
+                Abrir en Waze
             </button>
           </div>
         </div>
